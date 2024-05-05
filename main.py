a0afeceae502fd5187fdb313637e90a54f251b83
unsubstantiated-Script <jusfulton@gmail.com>
2024-05-04 18:49:44 -0700

    Card verification and password authentication in place.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas
 
 df = pandas.read_csv('hotels.csv', dtype={"id": str})
+df_cards = pandas.read_csv('cards.csv', dtype=str).to_dict(orient='records')
+df_cards_security = pandas.read_csv('card_security.csv', dtype=str)
 
 
 class Hotel:
@@ -38,15 +40,46 @@ class ReservationTicket:
         return content
 
 
-print(df)
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
 
+    def validate(self, expiration, holder, cvc):
+        # creating a dictionary
+        card_data = {"number": self.number, "expiration": expiration, "cvc": cvc, "holder": holder}
+
+        # Verifying card against stored values in CSV.
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+# Extending Class by passing the parent in. Easy Peasy
+class SecureCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security['number'] == self.number, 'password'].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
+
+print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    credit_card = SecureCreditCard(number="1234567890123456")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("Sorry, wrong password.")
+    else:
+        print("There was a problem with your payment method.")
 else:
     print("No hotel available")
